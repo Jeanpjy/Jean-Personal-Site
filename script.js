6464b83836ea15150d68c666909721d9e133d382
Jean Pan <111933900+Jeanpjy@users.noreply.github.com>
2023-05-18 15:19:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+    document.addEventListener("click", hideDropdown);
+  }
+  
+  function hideDropdown(event) {
+    
+   
+  var dropdown = document.getElementById("myDropdown");
+    if (!event.target.matches('.dropbtn')) {
+      dropdown.classList.remove("show");
+  
+      // Remove event listener from document
+      document.removeEventListener("click", hideDropdown);
+    }
+  }
